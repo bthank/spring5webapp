bfbf6a1d593c542849f6cf69fe62e3d1e49074d9
bthank <bthankachan@yahoo.com>
2021-08-11 22:13:04 -0400

    Added toString, hashCode and equals override methods to Book and Author entity classes

--- spring5webapp/src/main/java/com/binu/springframework/spring5webapp/domain/Author.java
+++ spring5webapp/src/main/java/com/binu/springframework/spring5webapp/domain/Author.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Author {
@@ -15,7 +16,7 @@ public class Author {
 	private Long id;
 	private String firstName;
 	private String lastName;
-	
+	@ManyToMany(mappedBy = "authors")
 	private Set<Book> books;
 	
 	// JPA requires a 0 arg constructor, so make sure it exists
@@ -61,6 +62,36 @@ public class Author {
 
 	public void setBooks(Set<Book> books) {
 		this.books = books;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
 	}
 	
 	
